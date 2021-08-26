2363aaf2860d578afd52387c117eba5cb0518602
Marcello Haddeman <marcello@luyten.nl>
2021-08-26 11:13:05 +0200

    Basic Project Setup

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use gdnative::prelude::*;
+
+#[derive(NativeClass)]
+#[inherit(Node)]
+pub struct HelloWorld;
+
+#[methods]
+impl HelloWorld {
+    fn new(_owner: &Node) -> Self {
+        HelloWorld
+    }
+
+    #[export]
+    fn _ready(&self, _owner: &Node) {
+        godot_print!("hello, world.");
+    }
+}
+
+fn init(handle: InitHandle) {
+    handle.add_class::<HelloWorld>();
+}
+
+godot_init!(init);
